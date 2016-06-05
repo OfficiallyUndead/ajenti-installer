55cbf2f63d25d95e244ea23c8b57146c93bdd5d4
Dean Cole <d3ancole1995@gmail.com>
2016-06-05 21:20:55 +0100

    Update installer.sh
    * Added sudo to commands

--- installer.sh
+++ installer.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-apt-get update && sudo apt-get install ajenti -y
-service ajenti restart
-apt-get install ajenti-v ajenti-v-nginx ajenti-v-mysql ajenti-v-php-fpm ajenti-v-mail ajenti-v-nodejs php5-mysql -y
-ufw allow 25
-ufw allow 80
-ufw allow 146
-ufw allow 443
-ufw allow 8000
+sudo apt-get update && sudo apt-get install ajenti -y
+sudo service ajenti restart
+sudo apt-get install ajenti-v ajenti-v-nginx ajenti-v-mysql ajenti-v-php-fpm ajenti-v-mail ajenti-v-nodejs php5-mysql -y
+sudo ufw allow 25
+sudo ufw allow 80
+sudo ufw allow 146
+sudo ufw allow 443
+sudo ufw allow 8000
