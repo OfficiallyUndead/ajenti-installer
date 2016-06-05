b761ad8d31b4c545ee49f5831fd204bb06961f68
Dean Cole <d3ancole1995@gmail.com>
2016-06-05 21:07:47 +0100

    Updated installer.sh
    * Added -y to general packages installation

--- installer.sh
+++ installer.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-apt-get update -y
-apt-get upgrade -y
-apt-get install zip unzip samba mc htop
-wget http://repo.ajenti.org/debian/key -O- | sudo apt-key add -
-echo "deb http://repo.ajenti.org/ng/debian main main ubuntu" | sudo tee -a /etc/apt/sources.list
 apt-get update && sudo apt-get install ajenti -y
 service ajenti restart
 apt-get install ajenti-v ajenti-v-nginx ajenti-v-mysql ajenti-v-php-fpm ajenti-v-mail ajenti-v-nodejs php5-mysql -y
